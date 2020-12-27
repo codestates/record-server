d7afe6dba892b439f06fcf00b9190d1e61fe1b97
dolsotbap <leejeongro1992@gmail.com>
2020-12-28 02:37:11 +0900

    [server]/38/sequelize_db_schema_migration-3

--- server/config/config.js
+++ server/config/config.js
@@ -2,13 +2,13 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": 'record',
-    "host": process.env.HOST,
-    "dialect": "mysql"
-  },
+  // "development": {
+  //   "username": process.env.USER,
+  //   "password": process.env.PASSWORD,
+  //   "database": 'record',
+  //   "host": process.env.HOST,
+  //   "dialect": "mysql"
+  // },
   // "test": {
   //   "username": "root",
   //   "password": null,
@@ -16,11 +16,12 @@ module.exports = {
   //   "host": "127.0.0.1",
   //   "dialect": "mysql"
   // },
-  // "production": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_production",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // }
+  "production": {
+      "username": process.env.USER,
+      "password": process.env.PASSWORD,
+      "database": 'record',
+      "host": process.env.HOST,
+      "port": process.env.PORT,
+      "dialect": "mysql"
+    }
 }
