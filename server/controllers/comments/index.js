01821d8c15e35b55357f606ad3b4973358d4c8f8
dolsotbap <leejeongro1992@gmail.com>
2020-12-29 16:38:42 +0900

    [server]/41and42/users_register

---
+++ server/controllers/comments/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+// create : 댓글등록
+// / :id / update : 댓글수정
+// / :id / remove : 댓글삭제
+  };
---
+++ server/controllers/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  usersController: require('./users'),
+  postsController: require('./posts'),
+  commentsController: require('./comments'),
+  tagsController: require('./tags'),
+};
+
